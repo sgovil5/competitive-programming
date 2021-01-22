782f2223596d71ac4de56b50eb42449d646b6131
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2021-01-22 13:53:27 -0500

    FF, I NEED MORE FF

--- USACO/2015-2016/December/Silver/SwitchingOnTheLights.cpp
+++ USACO/2015-2016/December/Silver/SwitchingOnTheLights.cpp
@@ -5,12 +5,14 @@ ofstream fout("lightson.out");
 
 const int MXN = 101;
 int n,m;
+// Cool data structure amirite?
 map<pair<int,int>, vector<pair<int,int>>> lights;
 bool on[MXN][MXN];
 bool visited[MXN][MXN];
 int ans = 0;
 const int xd[4] = {0,1,0,-1}, yd[4] = {1,0,-1,0};
 
+// Check if a room is adj to a room that has alr been vis
 bool adjVis(int x, int y){
   for(int i=0; i<4; i++){
     if(on[x+xd[i]][y+yd[i]] && visited[x+xd[i]][y+yd[i]]) return true;
@@ -21,8 +23,10 @@ bool adjVis(int x, int y){
 void ff(int x, int y){
   if(x<0 || x>=n || y<0 || y>=n || !on[x][y] || visited[x][y]) return;
   visited[x][y] = true;
+  // Switch on all the lights
   for(auto elem: lights[{x,y}]){
     on[elem.first][elem.second] = true;
+    // If room is adj to already visited node, Bessie can travel there and ff
     if(adjVis(elem.first, elem.second)) ff(elem.first, elem.second);
   }
   for(int i=0; i<4; i++){
@@ -41,6 +45,7 @@ int main(){
   }
   for(int i=0; i<=n; i++) for(int j=0; j<=n; j++){ on[i][j] = false; visited[i][j] = false;}
   on[0][0] = true;
+  // When you spend months to realize that you actually have to start with just 0,0 and nothing else
   ff(0,0);
   for(int i=0; i<=n; i++){
     for(int j=0; j<=n; j++){
