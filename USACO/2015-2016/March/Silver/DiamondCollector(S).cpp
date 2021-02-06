5e1159782a3922f82360ba4df0aba5ce0a9042f4
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2021-02-06 15:01:51 -0500

    Two Pointers, but this time I actually understand

--- USACO/2015-2016/March/Silver/DiamondCollector(S).cpp
+++ USACO/2015-2016/March/Silver/DiamondCollector(S).cpp
@@ -3,28 +3,33 @@ using namespace std;
 ifstream fin("diamond.in");
 ofstream fout("diamond.out");
 
-int main() {
-  int n,k;
-  fin>>n>>k;
-  int arr[n];
-  for(int i=0; i<n; i++){
-    fin>>arr[i];
-  }
+int arr[50003];
+int fit[50003] = {1};
+int fitRight[50003];
+
+int main(){
+  //Setup
+  int n,k; fin>>n>>k;
+  for(int i=0; i<n; i++) fin>>arr[i];
   sort(arr, arr+n);
-  int fitLeft[n];
-  int l=0, r = 0;
+
+  // Calculate how many each starting point can hold
+  for(int i=0; i<n; i++) fit[i]=1;
+  int l = 0, r = 0;
   for( ;l<n; l++){
     while(r<n && arr[r]<=arr[l]+k) r++;
-    fitLeft[l] = r-l;
+    fit[l] = r-l;
   }
-  int maxRight[n+1];
-  maxRight[n] = 0;
-  for(int i = n-1; i>=0; i--){
-    maxRight[i] = max(maxRight[i+1], fitLeft[i]);
-  }
-  int ans = 0;
+  
+  // Create array to calculate max of the 2nd collection
+  fitRight[n] = 0;
+  for(int i=n-1; i>=0; i--) fitRight[i] = max(fitRight[i+1], fit[i]);
+  
+  // Calculate answer
+  int maxAns = 0;
   for(int i=0; i<n; i++){
-    ans = max(maxRight[i+fitLeft[i]]+fitLeft[i], ans);
+    int ans=fit[i] + fitRight[i+fit[i]];
+    maxAns = max(maxAns, ans);
   }
-  fout<<ans;
+  fout<<maxAns;
 }
