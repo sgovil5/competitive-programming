3a5c253d9dcb0190ac403821de57beec1c9fa7d6
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2021-02-07 17:21:14 -0500

    Binary Search

--- USACO/2019-2020/December/Silver/Moobuzz.cpp
+++ USACO/2019-2020/December/Silver/Moobuzz.cpp
@@ -1,20 +1,27 @@
-#include <iostream>
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
 ifstream fin("moobuzz.in");
 ofstream fout("moobuzz.out");
-int main() {
-  long long n;
+
+using ll = long long;
+
+ll n;
+
+bool check(ll num){
+  ll place = (num/3) + (num/5) - (num/15);
+  return ((num-place)>=n);
+}
+
+ll fstTrue(ll lo, ll hi) {
+  for (hi ++; lo < hi; ) {
+    ll mid = (lo+hi)/2;
+    check(mid) ? hi = mid : lo = mid+1;
+  }
+  return lo;
+}
+
+int main(){
   fin>>n;
-  long long arr[8] = {1,2,4,7,8,11,13,14};
-  long long ans;
-  if(n%8==0){
-    ans= 15*(n%8)-1;
-  }
-  else{
-    long long mult = n/8;
-    long long add = n%8;
-    ans = 15*mult + arr[add-1];
-  }
-  fout<<ans;
+  ll hi = 1e18;
+  fout<<fstTrue(1, hi);
 }
