5465705f996e80997c7003d0b777f1c851917d24
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2021-02-08 18:01:15 -0500

    Two Pointers + Custom Sorting(Gets Testcase 11 WA)

---
+++ USACO/2019-2020/January/Silver/MountainView.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+ifstream fin("mountains.in");
+ofstream fout("mountains.out");
+
+#define int long long
+
+struct Mountain{
+  int left, right;
+};
+
+bool cmp(const Mountain& a, const Mountain& b){
+  if(a.left==b.left) return a.right<b.right;
+  return a.left<b.left;
+}
+
+int n;
+vector<Mountain> v;
+
+signed main(){
+  fin>>n;
+  for(int i=0; i<n; i++){
+    int x,y;
+    fin>>x>>y;
+    v.push_back({ x-y, x+y});
+  }
+  sort(begin(v), end(v), cmp);
+  int seen=0;
+  int l = 0, r = 0;
+  for(l=0; l<n; l++){
+    while(v[l].right>=v[r].right && r<n){
+      r++;
+    }
+    if(v[l].right<v[r].right){
+      seen++;
+      l = r;
+      l--;
+    }
+  }
+  fout<<seen+1;
+}
