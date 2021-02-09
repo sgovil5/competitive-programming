118b51763cf8d7e9fc9a6c2fcf3f2bf790b225dc
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2021-02-08 20:27:43 -0500

    2P + Custom Sort(nave orz)

--- USACO/2019-2020/January/Silver/MountainView.cpp
+++ USACO/2019-2020/January/Silver/MountainView.cpp
@@ -6,11 +6,12 @@ ofstream fout("mountains.out");
 #define int long long
 
 struct Mountain{
+  // left and right x coordinates of the base at y = 0
   int left, right;
 };
 
 bool cmp(const Mountain& a, const Mountain& b){
-  if(a.left==b.left) return a.right<b.right;
+  if(a.left==b.left) return a.right>b.right;
   return a.left<b.left;
 }
 
@@ -18,13 +19,16 @@ int n;
 vector<Mountain> v;
 
 signed main(){
+  // Input
   fin>>n;
   for(int i=0; i<n; i++){
     int x,y;
     fin>>x>>y;
+    // Convert to look at bases instead of peaks
     v.push_back({ x-y, x+y});
   }
   sort(begin(v), end(v), cmp);
+  // Two Pointers
   int seen=0;
   int l = 0, r = 0;
   for(l=0; l<n; l++){
