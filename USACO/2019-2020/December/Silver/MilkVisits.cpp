bba5d49805e2f4da468518986b2a2254f8279f4a
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2021-02-19 16:40:19 -0500

    Trees + DFS(Count components)

--- USACO/2019-2020/December/Silver/MilkVisits.cpp
+++ USACO/2019-2020/December/Silver/MilkVisits.cpp
@@ -3,42 +3,60 @@ using namespace std;
 ifstream fin("milkvisits.in");
 ofstream fout("milkvisits.out");
 
-const int MXN = 1e5 + 5;
-int n,m;
-string s;
-vector<int> adj[MXN];
-char breed[MXN];
-int component[MXN];
-int componentNum;
+const int MX = 1e5+5;
 
-void dfs(int node){
-  if(component[node]) return;
-  component[node] = componentNum;
+int n,m;
+int component[MX];
+vector<int> adj[MX];
+bool vis[MX];
+char type[MX];
+vector<pair<pair<int,int> , char>> queries;
+
+void dfs(int node, int comp){
+  vis[node] = true;
+  component[node] = comp;
   for(auto u: adj[node]){
-    if(breed[node]==breed[u]) dfs(u);
+    if(!vis[u] && type[node]==type[u]) dfs(u, comp);
   }
 }
 
-int main() {
-  fin>>n>>m>>s;
-  for(int i=1; i<=n; i++) breed[i] = s[i-1]; 
+int main(){
+  //Input
+  fin>>n>>m;
+  string s;
+  fin>>s;
+  for(int i=0; i<n; i++) type[i+1] = s[i];
   for(int i=0; i<n-1; i++){
-    int a,b;
-    fin>>a>>b;
-    adj[a].push_back(b);
-    adj[b].push_back(a);
-  }
-  for(int i=1; i<=n; i++){
-    if(!component[i]){
-      componentNum++;
-      dfs(i);
-    }
+    int a,b; fin>>a>>b;
+    adj[a].push_back(b), adj[b].push_back(a);
   }
   for(int i=0; i<m; i++){
     int a,b;
     char c;
     fin>>a>>b>>c;
-    if(breed[a]==c || component[a]!=component[b]) fout<<1;
-    else fout<<0;
+    queries.push_back({{a,b},c});
+  }
+  fill(vis, vis+n+1, false);
+
+  //Calculate component
+  int currComp = 0;
+  for(int i=1; i<=n; i++){
+    if(!vis[i]){
+      dfs(i, ++currComp);
+    }
+  }
+
+  // Calculate queries
+  for(int i=0; i<m; i++){
+    int a = queries[i].first.first;
+    int b = queries[i].first.second;
+    char c = queries[i].second;
+    if(component[a] == component[b]){
+      if(c==type[a]) fout<<1;
+      else fout<<0;
+    }
+    else{
+      fout<<1;
+    }
   }
 }
