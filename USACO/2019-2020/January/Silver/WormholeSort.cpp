50b6c98142071814158b3520c56230c85c5d5688
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2021-03-29 16:24:02 -0400

    DFS(Connected Components) + Binary Search

--- USACO/2019-2020/January/Silver/WormholeSort.cpp
+++ USACO/2019-2020/January/Silver/WormholeSort.cpp
@@ -5,58 +5,68 @@ ofstream fout("wormsort.out");
 
 #define f first
 #define s second
+typedef pair<int,int> pi;
 
-const int MX = 1e5+2;
+const int MX = 1e5 + 5;
 int n,m;
-vector<pair<int,int>> adj[MX]; //a,b,w
-vector<int> arr(MX), component(MX);
+int startPos[MX];
+int component[MX];
+vector<pi> paths[MX]; // node, w
 
-void dfs(int node, int label, int w){
-  component[node] = label;
-  for(auto u: adj[node]){
-    if(!(u.s<w || component[u.f] !=-1)) dfs(u.f, label, w);
+void dfs(int node, int compNum, int w){
+  // add currently traversed node to current component being looked at
+  component[node] = compNum;
+
+  for(auto u: paths[node]){
+    // visited if component has been considered before
+    bool vis = (component[u.f]==-1 ? false : true);
+    // make sure that current pathway meets width requirements
+    if(!vis && u.s>=w) dfs(u.f, compNum, w);
   }
 }
 
-bool check(int width){
-  fill(component.begin(), component.end(), -1);
-  int label = -1;
+bool check(int w){
+  // start component again for each w we check because paths change
+  fill(begin(component), end(component), -1);
+  int currComp = -1;
+  
+  // perform dfs to find all components that nodes belong to
   for(int i=0; i<n; i++){
-    if(component[i]==-1) dfs(i, ++label, width);
+    if(component[i]==-1) dfs(i, ++currComp, w);
   }
-  bool ans = true;
+
+  // make sure swapped numbers are in the same components so they can travel back
+  bool res = true;
   for(int i=0; i<n; i++){
-    if(component[i]!=component[arr[i]]) ans = false;
+    if(component[i]!=component[startPos[i]]) res = false;
   }
-  return ans;
+  return res;
+}
+
+// Find the greatest width for which its possible
+int lastTrue(int lo, int hi) {
+	for (--lo; lo < hi; ) {
+		int mid = lo+(hi-lo+1)/2;
+		if (check(mid)) lo = mid;
+		else hi = mid-1;
+	}
+	return lo;
 }
 
 int main(){
   fin>>n>>m;
-  for(int i=0; i<n; i++) {
-    fin>>arr[i];
-    arr[i]--;
+  for(int i=0; i<n; i++){
+    fin>>startPos[i];
+    startPos[i]--;
   }
   for(int i=0; i<m; i++){
     int a,b,w;
     fin>>a>>b>>w;
-    a--, b--;
-    adj[a].push_back({b,w});
-    adj[b].push_back({a,w});
+    a--,b--;
+    paths[a].push_back({b,w});
+    paths[b].push_back({a,w});
   }
-  int sol = -1;
-  int lo = 1, hi = 1e9 + 1;
-  int top = hi;
-  while (lo <= hi) {
-    int mid = lo + (hi - lo)/2;
 
-    if (check(mid)) {
-      sol = max (sol, mid);
-      lo = mid + 1;
-    } 
-    else {
-      hi = mid - 1;
-    }
-  }
-  fout<<(sol == top ? -1 : sol);
-} 
+  int ans = lastTrue(0, 1000000001);
+  fout<<(ans == 1000000001 ? -1 : ans);
+}
