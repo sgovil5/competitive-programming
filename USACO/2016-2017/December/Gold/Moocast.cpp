ce44d73ddfa7135217325382947496206d12d620
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2021-05-03 17:17:04 -0400

    Resolved, DFS+BinSearch(first Gold Problem)

--- USACO/2016-2017/December/Gold/Moocast.cpp
+++ USACO/2016-2017/December/Gold/Moocast.cpp
@@ -3,51 +3,65 @@ using namespace std;
 ifstream fin("moocast.in");
 ofstream fout("moocast.out");
 
+#define f first
+#define s second
+
+const int MX = 1005;
+
 int n;
-int pos[1002][2];
-bool visited[1002];
+vector<pair<int,int>> points;
+bool vis[MX];
 
 void dfs(int node, vector<int> adj[]){
-  visited[node] = true;
+  vis[node] = true;
   for(auto u: adj[node]){
-    if(!visited[u]) dfs(u, adj);
+    if(!vis[u]) dfs(u, adj);
   }
 }
 
-bool check(int r){
-  fill(visited, visited+n, false);
-  vector<int> adj[n];
+bool check(int maxDist){
+  vector<int> adj[MX];
+
   for(int i=0; i<n; i++){
     for(int j=0; j<n; j++){
-      if(pow(pos[j][0]-pos[i][0],2) + pow(pos[j][1]-pos[i][1],2)<=r){
+      int dist = pow(points[i].f-points[j].f,2) + pow(points[i].s-points[j].s,2);
+      if(dist<=maxDist){
         adj[i].push_back(j);
         adj[j].push_back(i);
       }
     }
   }
+
+  for(int i = 0; i<MX; i++) vis[i] = false;
+
   dfs(0, adj);
+
   bool possible = true;
-  for(int i=0; i<n; i++){
-    if(visited[i]==false) possible = false;
+  for(int i=0; i<n; i++) {
+    if(!vis[i]){
+      possible = false;
+    }
   }
+
   return possible;
 }
 
-int fstTrue(int lo, int hi) {
-    // returns smallest x in [lo,hi] that satisfies f
-    // hi+1 if no x satisfies f
-    for (hi ++; lo < hi; ) {
-        int mid = (lo+hi)/2;
-        check(mid) ? hi = mid : lo = mid+1;
-    }
-    return lo;
+int firstTrue(int lo, int hi){
+	for (hi ++; lo < hi; ) {
+		int mid = lo+(hi-lo)/2;
+		if (check(mid)) hi = mid;
+		else lo = mid+1;
+	}
+	return lo;
 }
 
-int main() {
+int main(){
   fin>>n;
   for(int i=0; i<n; i++){
-    fin>>pos[i][0]>>pos[i][1];
+    int a,b;
+    fin>>a>>b;
+    points.push_back({a,b});
   }
-  int ans = fstTrue(0, 1000000000);
-  fout<<ans;
+
+  fout<<firstTrue(0, 1000000000);
 }
