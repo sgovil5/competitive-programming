c4b357e92a5f532e6c58d28eea23fae43cf22efe
horsefeedapples <54728068+horsefeedapples@users.noreply.github.com>
2021-05-25 14:00:16 -0400

    DSU, but I actually solve it

--- CSES/Graph Algorithms/RoadConstruction.cpp
+++ CSES/Graph Algorithms/RoadConstruction.cpp
@@ -1,35 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-using vi = vector<int>;
-
 struct DSU {
-  vi e; void init(int N) { e = vi(N,-1); }
-  // get representive component, uses path compression
-  int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
-  bool sameSet(int a, int b) { return get(a) == get(b); }
-  int size(int x) { return -e[get(x)]; }
-  bool unite(int x, int y) { // union by size
-    x = get(x), y = get(y); if (x == y) return 0;
-    if (e[x] > e[y]) swap(x,y);
-    e[x] += e[y]; e[y] = x; return 1;
-  }
+	vector<int> e; void init(int N) { e = vector<int>(N,-1); }
+	// get representive component, uses path compression
+	int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
+	bool sameSet(int a, int b) { return get(a) == get(b); }
+	int size(int x) { return -e[get(x)]; }
+	bool unite(int x, int y) { // union by size
+		x = get(x), y = get(y); if (x == y) return 0;
+		if (e[x] > e[y]) swap(x,y);
+		e[x] += e[y]; e[y] = x; return 1;
+	}
 };
 
+int n, m;
+DSU d;
 int main(){
-  int n, m;
   cin>>n>>m;
-  DSU dsu;
-  dsu.init(n+1);
-  int cc = n;
-  int maxSz = 1;
+  d.init(n);
+  int numCmp = n, mxSz = 1;
   for(int i=0; i<m; i++){
-    int a,b;
-    cin>>a>>b;
-    if(dsu.unite(a, b)){
-      cc--;
-      maxSz = max(maxSz, dsu.size(a));
-    }
-    cout<<cc<<" "<<maxSz<<endl;
+    int a,b; cin>>a>>b;
+    a--,b--;
+    if(!d.sameSet(a,b)) numCmp--;
+    d.unite(a,b);
+    mxSz = max(mxSz, d.size(a));
+    cout<<numCmp<<' '<<mxSz<<endl;
   }
 }
