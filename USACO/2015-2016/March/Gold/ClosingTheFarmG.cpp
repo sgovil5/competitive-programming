d818c4b26946a39b051bf5c7d11a19cbec26d9c9
horsefeedapples <54728068+horsefeedapples@users.noreply.github.com>
2021-06-01 17:35:39 -0400

    DSU, but I can solve it :smile:

--- USACO/2015-2016/March/Gold/ClosingTheFarmG.cpp
+++ USACO/2015-2016/March/Gold/ClosingTheFarmG.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 void setIO(string fileName = "") {
-	//ios_base::sync_with_stdio(0); cin.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0);
 	if((int)fileName.size()) {
 		freopen((fileName+".in").c_str(), "r", stdin);
 		freopen((fileName+".out").c_str(), "w", stdout);
@@ -23,12 +24,14 @@ struct DSU {
 	}
 };
 
-const int MX = 200010;
-int n,m, cmps = 0;
-int closeOrder[MX], closePlace[MX], u[MX], v[MX];
+const int MX = 200005;
+int n,m;
+// closePos[i] represents which position it closed at
+// For example, if closePos[3] = 2, then farm 3 was the 3rd(0-index) farm to be closed 
+int closeOrder[MX], closePos[MX], u[MX], v[MX];
 bool res[MX];
-vector<int> adj[MX];
 DSU d;
+vector<int> adj[MX];
 
 int main(){
     setIO("closing");
@@ -38,24 +41,33 @@ int main(){
     }
     for(int i=0; i<n; i++){
         cin>>closeOrder[i];
-        closePlace[closeOrder[i]] = i;
+        closePos[closeOrder[i]] = i;
     }
+
     d.init(n+1);
 
     for(int i=0; i<m; i++){
-        if(closePlace[u[i]]>closePlace[v[i]]) adj[v[i]].push_back(u[i]);
+        // If u[i] closes after v[i] then there will not be a connection between u[i] and v[i] once v[i] closes
+        // Thus, we add only to the adj of v[i] because this connection will be irrelevant after
+        if(closePos[u[i]]>closePos[v[i]]) adj[v[i]].push_back(u[i]);
         else adj[u[i]].push_back(v[i]);
     }
 
+    int currComponents = 0;
+    // We want to go backwards in terms of when the farm closes
+    // This ensures that we can add "connected components" using DSU 
+    // Deleting components is hard with DSU
     for(int i=n-1; i>=0; i--){
-        int u = closeOrder[i]; cmps++;
-        for(auto v: adj[u]){
-            if(!d.sameSet(u,v)){
-                d.unite(u,v); cmps--;
+        int currNode = closeOrder[i];
+        currComponents++;
+        for(auto a: adj[currNode]){
+            if(!d.sameSet(currNode, a)){
+                // For each new farm we can connect with, we have 1 less component
+                d.unite(a, currNode); currComponents--;
             }
         }
-        res[i] = (cmps<=1);
+        // Make sure everything can fit into 1 component(group)
+        res[i] = (currComponents<=1);
     }
-
-    for(int i=0; i<n; i++) cout<<(res[i] ? "YES" : "NO")<<endl;
+    for(int i=0; i<n; i++)cout<<(res[i] ? "YES" : "NO")<<endl;
 }
