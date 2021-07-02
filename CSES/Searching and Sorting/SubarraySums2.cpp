c94b21348e45a0aa9ed848b3b2304e8804987c16
Shitij Govil <shitijgovil@gmail.com>
2021-07-01 22:26:52 -0400

    Psums; Maps>Multisets

--- CSES/Searching and Sorting/SubarraySums2.cpp
+++ CSES/Searching and Sorting/SubarraySums2.cpp
@@ -1,21 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+#include <map>
 using namespace std;
+using ll = long long;
 
-int main() {
-  long long n,t;
-  cin>>n>>t;
-  long long arr[n];
-  long long prefix[n+1];
-  prefix[0] = 0;
-  for(long long i=0; i<n; i++){
-    cin>>arr[i];
-    prefix[i+1]=arr[i]+prefix[i];
+void setIO(){
+  ios_base::sync_with_stdio(0); cin.tie(0);
+}
+
+int main(){
+  setIO();
+  int n, x; cin>>n>>x;
+  int arr[200005];
+  for(int i=1; i<=n; i++) cin>>arr[i];
+  ll prefix[200005]; prefix[0] = 0;
+  map<ll, int> visBefore; visBefore[0] = 1;
+  ll ans = 0;
+  for(int i=1; i<=n; i++){
+    prefix[i] = prefix[i-1] + arr[i];
+    ans+=visBefore[prefix[i]-x];
+    visBefore[prefix[i]]++;
+    //cout<<ans<<" "<<prefix[i]<<endl;
   }
-  multiset<long long> valuesBefore;
-  long long sums = 0;
-  for(long long i=0; i<=n; i++){
-    sums+=valuesBefore.count(prefix[i]-t);
-    valuesBefore.insert(prefix[i]);
-  }
-  cout<<sums;
+  cout<<ans<<endl;
+  return 0;
 }
