738c295e2e5c22f39de8463d026c6760c5814c6f
Shitij Govil <shitijgovil@gmail.com>
2021-09-05 12:38:26 -0400

    Bipartite DFS

--- USACO/2018-2019/February/Silver/TheGreatRevegitation.cpp
+++ USACO/2018-2019/February/Silver/TheGreatRevegitation.cpp
@@ -1,60 +1,66 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-ifstream fin("revegetate.in");
-ofstream fout("revegetate.out");
 
 #define f first
 #define s second
 
-const int MX = 1e5+5;
+void setIO(string fileName = "") {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	if((int)fileName.size()) {
+		freopen((fileName+".in").c_str(), "r", stdin);
+		freopen((fileName+".out").c_str(), "w", stdout);
+	}
+}
 
+const int MX = 1e5+5;
 int n, m;
 vector<pair<int, char>> adj[MX];
-bool vis[MX], grass[MX];
-bool ok = true;
+bool vis[MX] = {false};
+int color[MX] = {0};
+bool possible = true;
 
-void dfs(int node, bool color){
+void dfs(int node, int prevColor, bool same){
   vis[node] = true;
-  grass[node] = color;
+  if(prevColor==0) color[node] = 1;
+  else{
+    if(same==true) color[node] = prevColor;
+    else color[node] = (prevColor==2 ? 1 : 2);
+  }
+
   for(auto u: adj[node]){
     if(vis[u.f]){
-      if(u.s=='S' && grass[u.f]!=color) ok = false;
-      else if(u.s=='D' && grass[u.f]==color) ok = false;
+      if(u.s=='S' && color[u.f]!=color[node]) possible = false;
+      else if(u.s=='D' && color[u.f]==color[node]) possible = false;
     }
     else{
-      if(u.s=='S'){
-        dfs(u.f, color);
-      }
-      else if(u.s=='D'){
-        dfs(u.f, !color);
-      }
+      dfs(u.f, color[node], (u.s=='S'?true:false));
     }
   }
 }
 
 int main(){
-  fin>>n>>m;
+  //setIO("revegetate");
+  cin>>n>>m;
   for(int i=0; i<m; i++){
-    char d;
-    int a,b;
-    fin>>d>>a>>b;
-    adj[a].push_back({b,d});
-    adj[b].push_back({a,d});
+    char x; int a, b; cin>>x>>a>>b;
+    adj[a].push_back({b, x});
+    adj[b].push_back({a, x});
   }
-  for(int i=0; i<=n; i++){vis[i] = false;}
-  if(!ok) fout<<0;
-  else{
-    int count = 0;
-    for(int i=1; i<=n; i++){
-      if(!vis[i]){
-        dfs(i, true);
-        count++;
-      }
+
+  int comp=0;
+  for(int i=1; i<=n; i++){
+    if(!vis[i]){
+      comp++;
+      dfs(i, 0, true);
     }
-    string ans = "1";
-    for(int i=0; i<count; i++){
-      ans+="0";
-    }
-    fout<<ans;
   }
+  
+  if(possible){
+    cout<<1;
+    for(int i=0; i<comp; i++) cout<<0;
+    cout<<endl;
+  }
+  else cout<<0;
+
 }
