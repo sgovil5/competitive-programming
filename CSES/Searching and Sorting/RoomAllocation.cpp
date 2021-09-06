d78fda744378b9d699de50617e3bd36dadadab96
Shitij Govil <shitijgovil@gmail.com>
2021-09-06 16:23:58 -0400

    Goofy Priority Queues and Sorting

--- CSES/Searching and Sorting/RoomAllocation.cpp
+++ CSES/Searching and Sorting/RoomAllocation.cpp
@@ -1,61 +1,58 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
-#include <algorithm>
 #include <queue>
-#include <algorithm>
+#include <map>
 using namespace std;
+
+#define int long long
 
 #define f first
 #define s second
 
 int n;
-int ans[200005];
-vector<pair<pair<int,int>,int>> v; //arrival, departure, og index
+vector<pair<int, pair<int, int>>> v; //index, start, end
+priority_queue<pair<int, int>> q; //time, room
+map<int, int> m; //index, room no.
 
-int main(){
+bool cmp(pair<int, pair<int, int>>& a, pair<int, pair<int, int>>& b){
+    if(a.s.f==b.s.f) return a.s.s<b.s.s;
+    return a.s.f<b.s.f;
+}
+
+signed main(){
     cin>>n;
     for(int i=0; i<n; i++){
-        int a,b;
-        cin>>a>>b;
-        v.push_back({{a,b},i});
+        int a,b; cin>>a>>b;
+        v.push_back({i, {a,b}});
     }
-    sort(begin(v), end(v));
-
-    priority_queue<pair<int,int>> pq; //departure, room number
-    int lastRoom = 0; //lastRoom represents the max room that has been used
+    sort(begin(v), end(v), cmp);
+    
+    int rooms = 0, lastRoom = 0;
     for(int i=0; i<n; i++){
-        if(pq.empty()){
+        if(q.empty()){
             lastRoom++;
-            /*
-                We have to use a negative in the pq
-                because pq only finds the max, and we need to 
-                reverse it. We also add the current room being used
-                so that we can calculate what rooms will be used
-                for future visitors
-            */
-            pq.push({-v[i].f.s, lastRoom});
-            // ans of the original index is what the current room is
-            ans[v[i].s] = lastRoom;
+            q.push({-v[i].s.s, lastRoom});
+            m[v[i].f]=lastRoom;
         }
         else{
-            // Room that becomes empty the first
-            pair<int,int> minTime = pq.top();
-            // We need to take the negative to counter the way we inserted stuff
-            // This is in the case that the room gets empty before the next visitor
-            if(-minTime.f<v[i].f.f){
-                pq.pop();
-                pq.push({-v[i].f.s, minTime.s});
-                ans[v[i].s] = minTime.s;
-            }
-            // This is in the case that we need to add a new room
-            else{
+            if(v[i].s.f<=-q.top().f){
                 lastRoom++;
-                pq.push({-v[i].f.s, lastRoom});
-                ans[v[i].s] = lastRoom;
+                q.push({-v[i].s.s, lastRoom});
+                m[v[i].f] = lastRoom;
+            }
+            else{
+                int curRoom = q.top().s; 
+                q.pop();
+                q.push({-v[i].s.s, curRoom});
+                m[v[i].f] = curRoom;
             }
         }
+        rooms = max(rooms, lastRoom);     
     }
-    
-    cout<<lastRoom<<endl;
-    for(int i=0; i<n; i++) cout<<ans[i]<<" ";
+
+    cout<<rooms<<endl;
+    for(int i=0; i<n; i++){
+        cout<<m[i]<<" ";
+    }
 }
