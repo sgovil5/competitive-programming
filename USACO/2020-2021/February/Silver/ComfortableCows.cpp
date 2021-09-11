9098ab1900a9f6f37b607514d035feacc28fe147
Shitij Govil <shitijgovil@gmail.com>
2021-09-11 15:45:47 -0400

    Finally solved it(FF, BFS?)

--- USACO/2020-2021/February/Silver/ComfortableCows.cpp
+++ USACO/2020-2021/February/Silver/ComfortableCows.cpp
@@ -1,58 +1,63 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <queue>
 using namespace std;
 
 #define f first
 #define s second
 
-// Template to move direction
-const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
-// 3000 because bounds may exceed 1000
-int present[3000][3000];
+const int MX = 3000;
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0 , -1};
 
-int main() {
-  int n, totalCows = 0;
+bool exists[MX][MX];
+
+int n;
+
+bool isComfy(int x, int y){
+  int num = 0;
+  for(int k=0; k<4; k++) if(exists[x+dx[k]][y+dy[k]]) num++;
+  return num==3;
+}
+
+int main(){
   cin>>n;
-  queue<pair<int,int>> q;
-  for(int initCows = 1; initCows<= n; initCows++){
-    int a,b;
-    cin>>a>>b;
-    // Make sure bounds don't go negative
-    a+=500, b+=500;
-    q.push({a,b});
+  
+  for(int i=0; i<MX; i++) for(int j=0; j<MX; j++) exists[i][j] = false;
 
-    auto evaluate = [&](int x, int y){
-      // If cow doesn't exist, we don't have need to check it neighbors
-      if(!present[x][y]) return;
-      int numAdj = 0;
-      // Count num adjacent cows
-      for(int d = 0; d<4; d++){
-        if(present[x+dx[d]][y+dy[d]]) ++numAdj;
-      }
+  int required = 0;
+  for(int i=1; i<=n; i++){
+    int x, y; cin>>x>>y; x+=500, y+=500;
 
-      if(numAdj==3){
-        // Find adjacent cow that doesn't exist to make cow uncomfortable
-        for(int d=0; d<4; d++){
-          pair<int,int> next = {x+dx[d], y+dy[d]};
-          // Add given cow to queue for processing
-          if(!present[next.f][next.s]) q.push(next);
+    queue<pair<int, int>> q;
+    q.push({x,y});
+
+    while(!q.empty()){
+      pair<int, int> p = q.front(); q.pop();
+
+      if(!exists[p.f][p.s]){
+        required++; exists[p.f][p.s]= true;
+        if(isComfy(p.f, p.s)){
+          for(int k=0; k<4; k++){
+            if(!exists[p.f+dx[k]][p.s+dy[k]]){
+              q.push({p.f+dx[k], p.s+dy[k]});
+            }
+          }
+        }
+
+        for(int k=0; k<4; k++){
+          int xx=p.f+dx[k], yy=p.s+dy[k];
+          if(exists[xx][yy]){
+            if(isComfy(xx, yy)){
+              for(int l=0; l<4; l++){
+                if(!exists[xx+dx[l]][yy+dy[l]]){
+                  q.push({xx+dx[l], yy+dy[l]});
+                }
+              }
+            }
+          }
         }
       }
-    };
-    // BFS
-    while(!q.empty()){
-      pair<int,int> curr = q.front();
-      q.pop();
-      // If the cow already exists, we have already processed it
-      if(!present[curr.f][curr.s]){
-        // Add a new cow
-        ++totalCows;
-        present[curr.f][curr.s] = true;
-        // Evaluate current cow and neighboring cows to make sure that none are comfortable
-        evaluate(curr.f, curr.s);
-        for(int d=0; d<4; d++) evaluate(curr.f+dx[d], curr.s+dy[d]);
-      }
     }
-    // Output because we need to find how many cows were added and total cows carryover from previous iterations
-    cout<<totalCows - initCows<<endl;
+    cout<<required-i<<endl;
   }
 }
