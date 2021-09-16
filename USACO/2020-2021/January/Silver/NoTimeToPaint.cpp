9bef7bde2310f073408cdd20821a3681b7d09da9
Shitij Govil <shitijgovil@gmail.com>
2021-09-16 12:49:05 -0400

    psums

--- USACO/2020-2021/January/Silver/NoTimeToPaint.cpp
+++ USACO/2020-2021/January/Silver/NoTimeToPaint.cpp
@@ -1,67 +1,42 @@
-// Ugh can't use bits/stdc++.h🙄
 #include <iostream>
-#include <vector>
-#include <map>
+#include <algorithm>
 #include <string>
 using namespace std;
 
+const int MX = 1e5+5;
+
 int n,q;
 string s;
+int min_since_prev[26];
+int prefix[MX], suffix[MX];
 
 int main(){
     cin>>n>>q>>s;
 
-    // Calculate and initialize prefix
-    vector<int> prefix(n), lastSeenPrefix(26, -1);
-
-    for(int i=0; i<n; i++){
-        if(i>0) prefix[i] = prefix[i-1];
-        if(lastSeenPrefix[s[i]-'A']==-1) prefix[i]++;
-        else{
-            bool isNewSegment = false;
-
-            // Spent 20 minutes to realize that I used i in the nested loop 😒
-            for(int j=0; j<s[i]-'A';j++){
-                if(lastSeenPrefix[j]>lastSeenPrefix[s[i]-'A']){
-                    isNewSegment = true;
-                    break;
-                }
-            }
-
-            if(isNewSegment) prefix[i]++;
-        }
-        lastSeenPrefix[s[i]-'A'] = i;
+    // Calculate Prefix
+    prefix[0] = 0;
+    for(int i=0; i<26; i++) min_since_prev[i] = -1;
+    for(int i=1; i<=n; i++){
+        prefix[i] =  prefix[i-1];
+        int cur = s[i-1]-'A';
+        if(min_since_prev[cur] < cur) prefix[i]++;
+        for(int c=0; c<26; c++) min_since_prev[c] = min(min_since_prev[c], cur);
+        min_since_prev[cur] = cur;
     }
 
-    // Calculate and initialize suffix
-    vector<int> suffix(n), lastSeenSuffix(26, n);
-
-    for(int i=n-1; i>=0; i--){
-        if(i<n-1) suffix[i] = suffix[i+1];
-        if(lastSeenSuffix[s[i]-'A'] == n) suffix[i]++;
-        else{
-            bool isNewSegement = false;
-            // Spent 20 minutes to realize that I used i in the nested loop 😒
-            for(int j=0; j<s[i]-'A'; j++){
-                if(lastSeenSuffix[j]<lastSeenSuffix[s[i]-'A']){
-                    isNewSegement = true;
-                    break;
-                }
-            }
-
-            if(isNewSegement) suffix[i]++;
-        }
-        lastSeenSuffix[s[i]-'A'] = i;
+    // Calculate Suffix
+    suffix[n+1] = 0;
+    for(int i=0; i<26; i++) min_since_prev[i] = -1;
+    for(int i=n; i>=1; i--){
+        suffix[i] = suffix[i+1];
+        int cur = s[i-1]-'A';
+        if(min_since_prev[cur]<cur) suffix[i]++;
+        for(int c=0; c<26; c++) min_since_prev[c] = min(min_since_prev[c], cur);
+        min_since_prev[cur] = cur;
     }
 
-    // Perform queries
-    while(q--){
-        int a,b, ans = 0;
-        cin>>a>>b;
-        a--,b--;
-        if(a>0) ans+=prefix[a-1];
-        if(b<n-1) ans+=suffix[b+1];
-        cout<<ans<<endl;
+    for(int i=0; i<q; i++){
+        int a, b; cin>>a>>b;
+        cout<<prefix[a-1]+suffix[b+1]<<endl;
     }
-    return 0;
 }
