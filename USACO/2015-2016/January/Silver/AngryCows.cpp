5409dd4efb78a31908df6cb1ee1387d47aa71f37
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2021-11-21 13:23:15 -0500

    Goofy lower bound and binary search

--- USACO/2015-2016/January/Silver/AngryCows.cpp
+++ USACO/2015-2016/January/Silver/AngryCows.cpp
@@ -3,34 +3,42 @@ using namespace std;
 ifstream fin("angry.in");
 ofstream fout("angry.out");
 
-int n,k;
-int pos[50005];
+int n, k;
+vector<int> v;
 
-bool check(int radius){
-  int cows = 1;
-  int leftNotExploded = pos[0];
-  for(int i=0; i<n; i++){
-    if(leftNotExploded +2 *radius<pos[i]){
-      cows++;
-      leftNotExploded = pos[i];
-    }
+bool check(int r){
+  int currCows = 0;
+  for(int currIndex=0; currIndex<n; currIndex++){
+    auto ind = lower_bound(v.begin(), v.end(), v[currIndex]+2*r);
+    v[ind-v.begin()]==v[currIndex]+2*r ?  ind = ind : ind = prev(ind);
+    currCows++;
+
+    currIndex = ind-v.begin();
   }
-  return cows<=k;
+
+  return currCows<=k;
 }
 
-int fstTrue(int lo, int hi){
-  for (hi ++; lo < hi; ) {
-        int mid = (lo+hi)/2;
-        check(mid) ? hi = mid : lo = mid+1;
-    }
-    return lo;
+int firstTrue(int lo, int hi) {
+	hi++;
+	while (lo < hi) {
+		int mid = lo + (hi - lo) / 2;
+		if (check(mid)) {
+			hi = mid;
+		} else {
+			lo = mid + 1;
+		}
+	}
+	return lo;
 }
 
-int main() {
+int main(){
   fin>>n>>k;
   for(int i=0; i<n; i++){
-    fin>>pos[i];
+    int x; fin>>x;
+    v.push_back(x);
   }
-  sort(pos, pos+n);
-  fout<<fstTrue(0,1000000000);
+  sort(v.begin(), v.end());
+
+  fout<<firstTrue(1, 1e9+5);
 }
