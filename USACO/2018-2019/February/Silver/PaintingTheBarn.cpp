fe78919907c6fedd2906168b69cf7e9d6c6a8c54
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2022-01-03 21:36:30 -0500

    1D prefix sums letsgo

--- USACO/2018-2019/February/Silver/PaintingTheBarn.cpp
+++ USACO/2018-2019/February/Silver/PaintingTheBarn.cpp
@@ -3,26 +3,30 @@ using namespace std;
 ifstream fin("paintbarn.in");
 ofstream fout("paintbarn.out");
 
-int main() {
-  int n,k;
-  fin>>n>>k;
-  int p[1002][1002];
-  while(n--){
-    int a,b,c,d;
-    fin>>a>>b>>c>>d;
-    a++, b++, c++, d++;
-    p[a][b]++;
-    p[c][d]++;
-    p[a][d]--;
-    p[c][b]--;
-  }
-  int cnt = 0;
-  for(int i=0; i<=1000; i++) p[i][0]=0, p[0][i]=0;
-  for(int i=1; i<=1000; i++){
-    for(int j=1; j<=1000; j++){
-      p[i][j]+=p[i-1][j]+p[i][j-1]-p[i-1][j-1];
-      if(p[i][j]==k) cnt++;
+const int MX = 1005;
+
+int main(){
+  int n,k; fin>>n>>k;
+  int g[MX][MX];
+  for(int i=0; i<MX; i++){
+    for(int j=0; j<MX; j++){
+      g[i][j] = 0;
     }
   }
-  fout<<cnt;
+
+  for(int i=0; i<n; i++){
+    int x1, y1, x2, y2; fin>>x1>>y1>>x2>>y2;
+    for(int j=y1; j<y2; j++){
+      g[x1][j]++, g[x2][j]--;
+    }
+  }
+
+  int ans = 0;
+  for(int i=1; i<MX; i++){
+    for(int j=1; j<MX; j++){
+      g[i][j] += g[i-1][j];
+      if(g[i][j]==k) ans++; 
+    }
+  }
+  fout<<ans<<endl;
 }
