cf7a50209c39ee271d378687fd11c8c3c74c016f
Shitij Govil <54728068+horsefeedapples@users.noreply.github.com>
2022-01-04 20:48:32 -0500

    1d psums. but with a 2d context

--- USACO/2013-2014/March/Silver/LazyCow.cpp
+++ USACO/2013-2014/March/Silver/LazyCow.cpp
@@ -1,55 +1,43 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+#include <bits/stdc++.h>
+using namespace std; 
+ifstream fin("lazy.in");
+ofstream fout("lazy.out");
 
-const int MX = 405;
-int n, k;
-int arr[MX][MX], prefix[MX][MX];
-
-void setIO(string fileName = "") {
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	if((int)fileName.size()) {
-		freopen((fileName+".in").c_str(), "r", stdin);
-		freopen((fileName+".out").c_str(), "w", stdout);
-	}
-}
-
-int main(){
-    setIO("lazy");
-    cin>>n>>k;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin>>arr[i][j];
-        }
+int main() {
+  int n, k; fin>>n>>k;
+  int g[n][n];
+  int prefix[n][n+1];
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      fin>>g[i][j];
     }
+  }
 
-    for(int i=0; i<n; i++){
-        prefix[i][0] = 0;
-        for(int j=1; j<=n; j++){
-            prefix[i][j] = prefix[i][j-1] + arr[i][j-1]; 
-        }
+  for(int i=0; i<n; i++){
+    prefix[i][0] = 0;
+  }
+  for(int i=0; i<n; i++){
+    for(int j=1; j<=n; j++){
+      prefix[i][j] = prefix[i][j-1] + g[i][j-1];
     }
+  }
+  
+  int ans = 0;
+  for(int i=0; i<n; i++){
+    for(int j=1; j<=n; j++){
+      int tempAns = 0;
 
-    int ans = 0;
+      for(int y=i; y>=i-k && y>=0; y--){
+        int radius = k-(i-y);
+        tempAns+=prefix[y][min(n, j+radius)]-prefix[y][max(1, j-radius)-1];
+      }
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            int sum=0;
-
-            int left = j-k+1, right =j+k+1; 
-            for(int l=i; l>=max(0, i-k); l--){
-                sum+=prefix[l][min(right, n)] - prefix[l][max(left, 1)-1];
-                left++, right--;
-            }
-
-            left = j-k+2, right = j+k;
-            for(int l=i+1; l<=min(n-1, i+k); l++){
-                sum+=prefix[l][min(right, n)] - prefix[l][max(left, 1)-1];
-                left++, right--;
-            }
-
-            ans = max(ans, sum);
-        }
+      for(int y=i+1; y<=i+k && y<n; y++){
+        int radius = k-(y-i);
+        tempAns+=prefix[y][min(n, j+radius)]-prefix[y][max(1, j-radius)-1];
+      }
+      ans = max(tempAns, ans);
     }
-    cout<<ans;
-}
+  }
+  fout<<ans<<endl;
+} 
