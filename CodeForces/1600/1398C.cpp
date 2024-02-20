77090e4e0e6b19f4ae85e28a5e63c75077a0a966
Shitij Govil <shitijgovil@gmail.com>
2024-02-20 11:39:09 -0500

    ugly ahh code + brainrot = misery

--- CodeForces/1600/1398C.cpp
+++ CodeForces/1600/1398C.cpp
@@ -1,31 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
-
-using ll = long long;
-using pii = pair<int,int>;
-
-void solve(){
-    int n; string s; cin>>n>>s;
-    vector<int> prefix(n+1, 0);
-    for(int i=1; i<=n; i++) prefix[i] = s[i-1]-'0';
-
-    for(int i=1; i<=n; i++) prefix[i]+=prefix[i-1];
-
-    map<int, ll> m;
-    for(int i=0; i<=n; i++) m[prefix[i]-i]++;
-
-    ll ans = 0;
-    for(auto elem: m){
-        if(elem.second>0) ans+=((elem.second)*(elem.second-1))/2;
+ 
+#define ll long long
+ 
+void solve() {
+    ll n; cin>>n;
+    vector<ll> v, psum;
+    set<ll> vals;
+    map<ll, ll> valCount;
+    psum.push_back(0);
+    for (ll i=1; i<=n; i++) {
+        char x; cin>>x;
+        psum.push_back(psum[i-1] + (x-'0'));
+        ll psumDiff = psum[i] - i;
+        if(vals.find(psumDiff) != vals.end()) {
+            valCount[psumDiff]++;
+        } else {
+            valCount[psumDiff] = 1;
+            vals.insert(psumDiff);
+        }
     }
-    cout<<ans<<endl;
+    
+    if(vals.find(0) != vals.end()) {
+        valCount[0]++;
+    } else {
+        valCount[0] = 1;
+    }
+ 
+    ll count = 0;
+    for (auto elem: vals) {
+        valCount[elem]--;
+        count += valCount[elem] + ((valCount[elem]) * (valCount[elem]-1))/2;
+    }
+    cout<<count<<endl;
 }
-
+ 
 int main(){
-    int tc; cin>>tc;
-    while(tc--) solve();
-    return 0;
+    ll t; cin>>t;
+    while(t--){
+        solve();
+    }
 }
